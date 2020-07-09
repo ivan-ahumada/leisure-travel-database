531e3ea6aeb0c04dc2948ba85fb65747f662815f
ivanhmd <57021827+ivan-ahumada@users.noreply.github.com>
2020-07-08 21:34:34 -0500

    update added

--- myDBupdate.sql
+++ myDBupdate.sql
@@ -4,7 +4,11 @@
 -- to all airline costs by 20%.
 -- This will drop some tickets below 200 dollars
 -- making Query #1 select different tuples
-
 UPDATE S20_2_RESERVES_AIRLINE
 SET airlineCost = airlineCost - (airlineCost * 0.2)
 
+-- The following update will update Natalia Perez's
+-- state of residency. 
+UPDATE S20_2_CUSTOMER
+SET state_resident = 'CO'
+WHERE custID = '000060000';
