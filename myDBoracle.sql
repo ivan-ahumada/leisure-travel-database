e74d2d8d4f93b565469062c0cc42b879f38e76b6
ivanhmd <57021827+ivan-ahumada@users.noreply.github.com>
2020-07-08 21:24:49 -0500

    STOOOPID

--- myDBoracle.sql
+++ myDBoracle.sql
@@ -267,15 +267,6 @@ INSERT ALL
         '000060000', 'AA 4368', '556356', 'Miami', 'Austin', 51.00)
 SELECT * FROM DUAL;
 
--------------------------------------------Updates
--- The following update will give a discount 
--- to all airline costs by 20%.
--- This will drop some tickets below 200 dollars
--- making Query #1 select different tuples
-
-UPDATE S20_2_RESERVES_AIRLINE
-SET airlineCost = airlineCost - (airlineCost * 0.2)
-
 ------------------------------------------Queries
 -- Query #1
 -- This query selects # of customers & their type that were born
@@ -314,9 +305,37 @@ GROUP BY CUBE(gender,custType);
 -- making Query #1 select different tuples
 
 UPDATE S20_2_RESERVES_AIRLINE
-SET airlineCost = airlineCost - (airlineCost * 0.2)
+SET airlineCost = airlineCost - (airlineCost * 0.2);
 
-----------------------------UPDATE TEST
+UPDATE S20_2_EMPLOYEES
+SET salary = salary + 1500;
+
+------------------------------------------Queries Part #2
+-- Query #1
+-- This query selects # of customers & their type that were born
+-- after January 1, 1994 spent over 200 dollars on their flight.
+-- 
+-- Helps us see what type of customers spend more & if age has an
+-- effect on flight ticket spending.
+SELECT COUNT(custID), custType
+FROM   S20_2_CUSTOMER
+WHERE  custID IN(
+    SELECT  custID
+    FROM    S20_2_RESERVES_AIRLINE
+    WHERE   airlineCost > 200)
+GROUP BY custType
+HAVING MIN(DOB) > '01-Jan-1994';
+
+-- Query #2
+-- This query shows which states spend the most on airline costs.
+SELECT state_resident, SUM(airlineCost), COUNT(*) QTY
+FROM S20_2_CUSTOMER c, S20_2_RESERVES_AIRLINE r
+WHERE c.custID = r.custID
+GROUP BY ROLLUP(state_resident);
+
+-- Query #3
+-- This query show what type of customer spends more money based
+-- gender and customer type.
 SELECT custType, gender, SUM(airlineCost), COUNT(*) QTY_Customers
 FROM S20_2_CUSTOMER c, S20_2_RESERVES_AIRLINE r
 WHERE c.custID = r.custID
